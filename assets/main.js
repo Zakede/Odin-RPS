bff5a78eed3ebf9be9c9415b776dabba5741f32d
zakede <crazygamingmannat1998@gmail.com>
2024-05-16 00:21:07 -0400

    Completed Code for game

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,41 @@
 const choices = ["rock", "paper", "scissors"];
 
-var humanScore  = 0;
+var humanScore = 0;
 var computerScore = 0;
 
-function getComputerChoice() {
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-}
-
-
-function gethumanChoice() { 
+function computerSelection() {
+    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
     alert("Choose rock, paper or scissors.");
-    let humanChoice = prompt("Enter your choice: ");
+    return computerSelection;
+
 }
+
 
 function playRound() {
-    getComputerChoice();
-    gethumanChoice();
 
-    
+    computerSelection();
+    let humanSelection = prompt("Enter your choice: ");
+
+    if (humanSelection === computerSelection) {
+        console.log("You Chose " + humanSelection + " and Computer Chose " + computerSelection + "!");
+        console.log("It's a tie!");
+
+    } else if (humanSelection === "rock" && computerSelection === "scissors" ||
+        humanSelection === "paper" && computerSelection === "rock" ||
+        humanSelection === "scissors" && computerSelection === "paper") {
+        humanScore++;
+        console.log("You Chose " + humanSelection + " and Computer Chose " + computerSelection + "!");
+        console.log("You Win, Your score " + humanScore);
+
+    } else {
+        computerScore++;
+        console.log("You Chose " + humanSelection + " and Computer Chose " + computerSelection + "!");
+        console.log("You Lose, Computer score " + computerScore);
+    }
+
+    console.log("Your Final Score is: " + humanScore);
+
 }
+
+
+playRound();
